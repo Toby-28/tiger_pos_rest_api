0a4777a84729c7c5b72bb3f0ee8f4b36d0f917a9
Hushnudbek <tobymarshal2802@gmail.com>
2022-07-28 23:09:23 +0500

    New Packages Added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ArticleModule } from './article/article.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, ArticleModule],
+  imports: [PrismaModule, ArticleModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
